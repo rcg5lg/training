4b36d5137df6f1a8063dd2a30853241e87c65bb1
Constantin Radu GEORGESCU <cgeorgescu@pentalog.com>
2018-01-23 15:56:23 +0200

    updated hero class for testing

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,4 +1,18 @@
 export class User {
+    constructor(id = 0, username = '', token = '', name = '', email = '', description = '', avatarUrl = '', age = 0,
+        currentProject = '', agency = '') {
+        this.id = id;
+        this.username = username;
+        this.token = token;
+        this.name = name;
+        this.email = email;
+        this.description = description;
+        this.avatarUrl = avatarUrl;
+        this.age = age;
+        this.currentProject = currentProject;
+        this.agency = agency;
+    }
+
     id: number;
     username: string;
     token: string;
@@ -8,5 +22,5 @@ export class User {
     avatarUrl: string;
     age: number;
     currentProject: string;
-    agency: string;
+    agency: String;
 }
